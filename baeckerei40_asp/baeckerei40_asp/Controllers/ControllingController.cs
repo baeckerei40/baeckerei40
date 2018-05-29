43c8f305991b337e2662a694cac891de4a221c05
Tobias Weiß <tobias@tobias-weiss.org>
2018-05-29 18:44:49 +0200

    working commit

---
+++ baeckerei40_asp/baeckerei40_asp/Controllers/ControllingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baeckerei40_asp.Controllers
+{
+    public class ControllingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
